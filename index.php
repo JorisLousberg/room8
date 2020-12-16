88878c1cd6de202e8254e4efe86e8a9b0cc68dde
galactorsus( <504972@vistacollege.nl>
2020-12-16 19:38:21 +0100

    basic layout
    added css file
    mainstyle.css
    
    added lay out structure:
    top (2 sections(left/right)
    middle(3 sections(borders/main)
    bottom(3 sections(left/middle/right)

--- index.php
+++ index.php
@@ -4,10 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="STYLESHEET" href="css/mainstyle.css" type="text/css">
+    <title>homepage</title>
 </head>
 <body>
-    
+    <div class="top">
+        <div class="upper_left">section1, open space→ </div>
+        <div class="upper_right">←open space ,section2</div>
+    </div>
+
+    <div class="middle">
+        <div class="middle_left">border1</div>
+        <div class="middle_main">main body</div>
+        <div class="middle_right">border2</div>
+    </div>
+    <div class="bottom">
+        <div class="lower_left">section1</div>
+        <div class="lower_middle">section2</div>
+        <div class="lower_right">section3</div>
+    </div>
 </body>
 </html>
 =======
