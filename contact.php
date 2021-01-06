6bb36f7082c6b1cd8fda98d2923e8203e8c5c36d
galactorsus( <504972@vistacollege.nl>
2021-01-06 19:50:08 +0100

    finished contact page (v1)
    added:
    about us discription
    google map
    opening times in a html table
    
    fixed: code mistake in layout on contact page

--- contact.php
+++ contact.php
@@ -4,21 +4,77 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="STYLESHEET" href="css/mainstyle.css" type="text/css">
-    <title>homepage</title>
+    <link rel="STYLESHEET" href="css/contact.css" type="text/css">
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="js/concact.js"></script>
+    <title>contact</title>
 </head>
 <body>
     <div class="top">
         <div class="upper_left">
-        <?php include_once 'includes/nav.inc.php'; ?>
+            <?php include_once 'includes/nav.inc.php'; ?>
         </div>
-        <div class="upper_right"></div>
+
+        <div class="upper_right">
+
+        </div>
     </div>
 
     <div class="middle">
         <div class="middle_left">border1</div>
-        <div class="middle_main"> <div class="groot"></div></div>
+
+        <div class="middle_main"> 
+            <div class="aboutus">
+                <p> <p>Over ons</p>
+                    <p>als de kantine van het vista college zorgen wij voor 
+                        een gezonde maaltijd. We bereiden het voedsel dagelijks voor zodat het altijd vers is.
+                        Als u vragen heeft waarbij ondersteuning nodig is, gelieve ons via onderstaande gegevens te contacteren.
+                    </p>
+                </p>
+                <table class="tg">
+                    <thead>
+                        <tr>
+                            <th class="tg-dk9t">maandag</th>
+                            <th class="tg-dk9t">dinsdag</th>
+                            <th class="tg-dk9t">woensdag</th>
+                            <th class="tg-dk9t">donderdag</th>
+                            <th class="tg-dk9t">vrijdag</th>
+                            <th class="tg-dk9t">zaterdag</th>
+                            <th class="tg-dk9t">zondag</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="tg-g967">12:00 -12:45</td>
+                            <td class="tg-g967">12:00 -12:45</td>
+                            <td class="tg-g967">12:00-12:45</td>
+                            <td class="tg-g967">12:00 -12:45</td>
+                            <td class="tg-g967">12:00 -12:45</td>
+                            <td class="tg-g967">gesloten</td>
+                            <td class="tg-g967">gesloten</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>Als u vragen heeft waarbij ondersteuning nodig is, gelieve ons via onderstaande gegevens te contacteren.</p>
+                <address>
+                    E-mail naar: <a href="mailto:notrealemail@hotmail.com">info@vistacollege.nl</a><br>
+                    of bel ons via: +32-456-5557-20 <mark class="note">"random gegenereerde nummer"</mark> <br>
+                    addres: <br>
+                    Sibemaweg 20 <br>
+                    6224DC Maastricht <br>
+                    Limburg, Nederland
+                </address>
+            </div>
+            <div class="map">
+                <div class="mapwrap">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.179036291988!2d5.714817473852619!3d50.84636881153734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0e98dd58c172b%3A0xb724894172214173!2sSibemaweg%2020%2C%206224%20DC%20Maastricht%2C%20Nederland!5e0!3m2!1snl!2sbe!4v1609956425829!5m2!1snl!2sbe" width="450" height="200" frameborder="1" style="border:1;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+            </div>
+        </div>
         <div class="middle_right">border2</div>
     </div>
+
     <div class="bottom">
         <div class="lower_left"></div>
         <div class="lower_middle"></div>
--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -4,4 +4,4 @@
         <div class="menuknop"><a href="menu.php" class="menuknopp">MENU</a></div>
         <div class="menuknop"><a href="faq.php" class="menuknopp">FAQ</a></div>
         <div class="menuknop"><a href="contact.php" class="menuknopp">CONTACT</a></div>
-        </nav></div>
+        </div></nav>
